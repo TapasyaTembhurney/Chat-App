09dac58f2263996719c2edc9c82eb6f3fe16320e
TapasyaTembhurney <tapasyatembhurney@gmail.com>
2024-05-19 12:19:11 +0530

    emoji box adjusted

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -3,11 +3,11 @@ import './Chat.css'
 import EmojiPicker from 'emoji-picker-react'
 const Chat = () => {
 
-    const [open,setOpen] = useState(false);
-    const [text,setText] = useState("");
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
 
-    const handleEmoji = e =>{
-        setText(prev => prev+e.emoji);
+    const handleEmoji = e => {
+        setText(prev => prev + e.emoji);
         setOpen(false)
     }
     console.log(text)
@@ -30,23 +30,25 @@ const Chat = () => {
                 </div>
             </div>
             <div className="center"></div>
-                <div className="bottom">
-                    <div className="icons">
-                        <img src="./img.png" alt="" />
-                        <img src="./camera.png" alt="" />
-                        <img src="./mic.png" alt="" />
-                    </div>
-                    <input type="text" placeholder='Type a message...'
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./img.png" alt="" />
+                    <img src="./camera.png" alt="" />
+                    <img src="./mic.png" alt="" />
+                </div>
+                <input type="text" placeholder='Type a message...'
                     value={text}
                     onChange={(e) => setText(e.target.value)}
-                    />
-                    <div className="emoji">
-                        <img src="./emoji.png" alt=""
-                        onClick={()=> setOpen((prev) => !prev)} />
-                        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                />
+                <div className="emoji">
+                    <img src="./emoji.png" alt=""
+                        onClick={() => setOpen((prev) => !prev)} />
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji} />
                     </div>
-                    <button className='sendButton'>Send</button>
                 </div>
+                <button className='sendButton'>Send</button>
+            </div>
         </div>
     )
 }
